06b3c1243bee5e72580a66b1195db5959d565b6a
eojin-dev <eojin-dev@naver.com>
2021-07-15 00:34:09 +0900

    feature. 페이지 슬라이딩 스위칭 (#8)

---
+++ frontend/src/pages/CategoryPage.js
@@ -0,0 +1,29 @@
+import { setState } from '../utils/globalObserver.js';
+import { pageState } from '../store/page.js';
+import HomePage from './HomePage.js';
+
+export default class CategoryPage {
+  constructor() {
+    this.$target = document.createElement('div');
+    this.setPageState = setState(pageState);
+    this.init();
+  }
+
+  init() {
+    this.$target.addEventListener('click', this.handleClick.bind(this));
+    this.render();
+  }
+
+  handleClick({ target }) {
+    if (!target.classList.contains('move-page')) return;
+
+    this.setPageState({ Page: HomePage, direction: 'right' });
+  }
+
+  render() {
+    this.$target.innerHTML = `
+      <h1>카테고리</h1>
+      <button class="move-page">홈 이동</button>
+    `;
+  }
+}
---
+++ frontend/src/pages/HomePage.js
@@ -0,0 +1,29 @@
+import { setState } from '../utils/globalObserver.js';
+import { pageState } from '../store/page.js';
+import CategoryPage from './CategoryPage.js';
+
+export default class HomePage {
+  constructor() {
+    this.$target = document.createElement('div');
+    this.setPageState = setState(pageState);
+    this.init();
+  }
+
+  init() {
+    this.$target.addEventListener('click', this.handleClick.bind(this));
+    this.render();
+  }
+
+  handleClick({ target }) {
+    if (!target.classList.contains('move-page')) return;
+
+    this.setPageState({ Page: CategoryPage, direction: 'left' });
+  }
+
+  render() {
+    this.$target.innerHTML = `
+      <h1>우아마켓</h1>
+      <button class="move-page">카테고리 이동</button>
+    `;
+  }
+}
