41b0d3e7a77e5588ed5cb8f2152f29a6a998d6e5
hayoung <hayoung@adminui-MacBookPro.local>
2021-07-15 17:20:08 +0900

    feat. 홈페이지 product list 마크업  (#13)

---
+++ frontend/src/components/ProductList/index.js
@@ -0,0 +1,111 @@
+import ProductItem from './ProductItem';
+import { createElement } from '../../utils/dom';
+
+export default class ProductList {
+  // constructor({ products }) {
+  constructor() {
+    this.$target = createElement({ tagName: 'div', classNames: ['product-list'] });
+    this.products = products;
+
+    this.init();
+  }
+  init() {
+    this.render();
+  }
+  render() {
+    products.forEach((product) => {
+      this.$target.appendChild(new ProductItem({ product }).$target);
+    });
+  }
+}
+
+import testImg0 from '../../../public/img/ImageLarge-0.png';
+import testImg1 from '../../../public/img/ImageLarge-1.png';
+import testImg2 from '../../../public/img/ImageLarge-2.png';
+
+const products = [
+  {
+    imgUrl: testImg0,
+    title: '파란선풍기',
+    town: '구암동',
+    createdDate: new Date('2021.07.14'),
+    price: 24500,
+    commentCount: 1,
+    likeCount: 2,
+    isLiked: true,
+  },
+  {
+    imgUrl: testImg1,
+    title: '빈티지 밀크 글래스',
+    town: '회기동',
+    createdDate: new Date('2021.07.14'),
+    price: 158000,
+    commentCount: 1,
+    isLiked: false,
+  },
+  {
+    imgUrl: testImg2,
+    title: '입사귀 포스터',
+    town: '역삼동',
+    createdDate: new Date('2021.07.14'),
+    price: 58000,
+    likeCount: 2,
+    isLiked: false,
+  },
+  {
+    imgUrl: testImg0,
+    title: '파란선풍기',
+    town: '구암동',
+    createdDate: new Date('2021.07.14'),
+    price: 24500,
+    commentCount: 1,
+    likeCount: 2,
+    isLiked: true,
+  },
+  {
+    imgUrl: testImg1,
+    title: '빈티지 밀크 글래스',
+    town: '회기동',
+    createdDate: new Date('2021.07.14'),
+    price: 158000,
+    commentCount: 1,
+    isLiked: false,
+  },
+  {
+    imgUrl: testImg2,
+    title: '입사귀 포스터',
+    town: '역삼동',
+    createdDate: new Date('2021.07.14'),
+    price: 58000,
+    likeCount: 2,
+    isLiked: false,
+  },
+  {
+    imgUrl: testImg0,
+    title: '파란선풍기',
+    town: '구암동',
+    createdDate: new Date('2021.07.14'),
+    price: 24500,
+    commentCount: 1,
+    likeCount: 2,
+    isLiked: true,
+  },
+  {
+    imgUrl: testImg1,
+    title: '빈티지 밀크 글래스',
+    town: '회기동',
+    createdDate: new Date('2021.07.14'),
+    price: 158000,
+    commentCount: 1,
+    isLiked: false,
+  },
+  {
+    imgUrl: testImg2,
+    title: '입사귀 포스터',
+    town: '역삼동',
+    createdDate: new Date('2021.07.14'),
+    price: 58000,
+    likeCount: 2,
+    isLiked: false,
+  },
+];
--- frontend/src/pages/HomePage/index.js
+++ frontend/src/pages/HomePage/index.js
@@ -2,11 +2,14 @@ import { setState } from '../../utils/globalObserver.js';
 import { pageState } from '../../store/page.js';
 import CategoryPage from '../CategoryPage';
 import { createElement } from '../../utils/dom.js';
+import TopBar from '../../components/TopBar';
+import ProductList from '../../components/ProductList';
 
 export default class HomePage {
   constructor() {
     this.$target = createElement({ tagName: 'div', classNames: ['page'] });
     this.setPageState = setState(pageState);
+
     this.init();
   }
 
@@ -22,9 +25,10 @@ export default class HomePage {
   }
 
   render() {
-    this.$target.innerHTML = `
-      <h1>우아마켓</h1>
-      <button class="move-page">카테고리 이동</button>
-    `;
+    const topBar = new TopBar();
+    const productList = new ProductList();
+    this.$target.innerHTML = '';
+    this.$target.appendChild(topBar.$target);
+    this.$target.appendChild(productList.$target);
   }
 }
