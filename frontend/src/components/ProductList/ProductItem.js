d5743bc3aed503fa4fba8bdc246864572593e496
hayoung <hayoung@adminui-MacBookPro.local>
2021-07-15 17:20:48 +0900

    feat. 상품 하트 또는 메뉴 조건부렌더링  (#13)

--- frontend/src/components/ProductList/ProductItem.js
+++ frontend/src/components/ProductList/ProductItem.js
@@ -3,26 +3,16 @@ import { getPassedTime, getWon } from '../../utils/convertToString.js';
 import { createElement } from '../../utils/dom';
 import bigHeart from '../../../public/assets/product/bigHeart.svg';
 import filledBigHeart from '../../../public/assets/product/filledBigHeart.svg';
+import menu from '../../../public/assets/product/product-menu.svg';
 import heart from '../../../public/assets/product/heart.svg';
 import comment from '../../../public/assets/product/comment.svg';
 
-import testImg from '../../../public/img/ImageLarge-0.png';
-const product = {
-  imgUrl: testImg,
-  title: '빈티지 밀크 글래스 어어어dkdkdkdkdkdkd',
-  town: '구암동',
-  createdDate: new Date('2021.07.14'),
-  price: 24500,
-  commentCount: 1,
-  likeCount: 2,
-  isLiked: true,
-};
-
+// TODO: 하트 버튼 상호작용
 export default class ProductItem {
-  // constructor({ product }) {
-  constructor() {
+  constructor({ product, isMyProduct }) {
     this.$target = createElement({ tagName: 'div', classNames: ['product-item'] });
     this.product = product;
+    this.isMyProduct = !!isMyProduct;
     this.init();
   }
 
@@ -51,7 +41,11 @@ export default class ProductItem {
             <div class="product-item-price">${won}</div>
           </div>
           <div>
-            <img src=${isLiked ? filledBigHeart : bigHeart} alt='like-button'/>
+            ${
+              this.isMyProduct
+                ? `<img src=${menu} alt='menu-button'/>`
+                : `<img src=${isLiked ? filledBigHeart : bigHeart} alt='like-button'/>`
+            }
           </div>
         </div>
         <div class="product-item-comment-like">
