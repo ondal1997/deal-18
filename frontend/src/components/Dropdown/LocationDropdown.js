5682637c20ea0745a5b73f4023f6a51a0989c757
hayoung <hayoung@adminui-MacBookPro.local>
2021-07-16 02:13:08 +0900

    feat. locationDropdown 구현  (#15)

---
+++ frontend/src/components/Dropdown/LocationDropdown.js
@@ -0,0 +1,39 @@
+import './style.scss';
+import { setState } from '../../utils/globalObserver';
+import { createElement } from '../../utils/dom';
+import { pageState } from '../../store/page';
+import { locationDropdownState } from '../../store/store';
+import LocationEditPage from '../../pages/LocationEditPage';
+
+export default class LocationDropdown {
+  constructor({ key }) {
+    this.$target = createElement({ tagName: 'div', classNames: ['dropdown', 'location-dropdown-wrapper'] });
+    this.key = key;
+    this.setIsOpen = setState(locationDropdownState);
+    this.setPage = setState(pageState);
+    this.init();
+  }
+
+  init() {
+    this.$target.addEventListener('click', this.handleClick.bind(this));
+    this.render();
+  }
+
+  render() {
+    this.$target.innerHTML = `
+        <div class="location-dropdown-item select-location">역삼동</div>
+        <div class="location-dropdown-item move-edit-page">내 동네 설정하기</div>
+    `;
+  }
+
+  handleClick({ target }) {
+    if (target.closest('.select-location')) this.setIsOpen(false);
+
+    if (target.closest('.move-edit-page')) this.setPage({ Page: LocationEditPage, direction: 'right' });
+  }
+
+  toggleLocationModal() {
+    const isOpen = getState(locationDropdownState);
+    this.setIsOpen(!isOpen);
+  }
+}
