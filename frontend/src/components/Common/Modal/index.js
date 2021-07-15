46a4555809c5ff70998e7b3e5adb041c85517eba
hayoung <hayoung@adminui-MacBookPro.local>
2021-07-16 02:12:29 +0900

    feat. dropdown 모달을 modal 공용 클래스로 구현 (#15)

--- frontend/src/components/Common/Modal/index.js
+++ frontend/src/components/Common/Modal/index.js
@@ -2,10 +2,10 @@ import './style.scss';
 import { createElement } from '../../../utils/dom';
 import { getState, setState, subscribe } from '../../../utils/globalObserver';
 
-export default class DropdownModal {
-  constructor({ contents, className, key }) {
+export default class Modal {
+  constructor({ View, className, key }) {
     this.$target = createElement({ tagName: 'div', classNames: ['dropdown-modal', className] });
-    this.contents = contents;
+    this.View = new View({ key }).$target; //상태에 따라 변경되는 사항이 없기 때문에 생성자에서 인스턴스 생성
     this.key = key;
     this.setIsOpen = setState(key);
 
@@ -14,38 +14,29 @@ export default class DropdownModal {
   init() {
     subscribe(this.key, this.toggleModal.bind(this));
     this.render();
-    this.toggleModal();
+    this.$target.style.display = 'none';
   }
+
   render() {
-    this.contents.forEach(({ value, clickCb }) => {
-      const $item = createElement({ tagName: 'div', classNames: ['dropdown-item'], value });
-      $item.addEventListener('click', clickCb);
-      this.$target.appendChild($item);
-    });
+    this.$target.appendChild(this.View);
   }
 
   toggleModal() {
     const isOpen = getState(this.key);
     this.$target.style.display = isOpen ? 'block' : 'none';
   }
-
-  handleClick({ target }) {
-    if (target.closest('dropdown-modal')) return;
-    this.setIsOpen(false);
-  }
 }
 
 /**
- * 공용으로 사용하기 위한 dropdown 모달
+ * 공용으로 사용하기 위한 모달
  *
- * 각 칸마다 하는 역할 (콜백함수)가 다르기 때문에 인자로 받아야 된다.
+ * 사용할 클래스를 View로 받아서 render 해준다.
  *
- * [{value:'역삼동',handleClick:함수}]
- * 위와 같은 형태로 받으면 될 것 같다.
  *
  * 또한 위치 역시 다르기 때문에 각 호출해주는 부분에서 class를 넘겨주어서 그것으로 위치를 컨트롤 하는 것이 좋아 보인다.
  *
  * 모달의 open 상태를 관리해야된다.
- *
  * 상위 컴포넌트에서 initState로 key를 만들어서 전달해주고 이를 활용해 state,setState를 활용한다.
+ *
+ * subscribe는
  */
