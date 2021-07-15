fb5a0208021b56ae016881df1bdbe11426563fda
hayoung <hayoung@adminui-MacBookPro.local>
2021-07-15 22:51:34 +0900

    feat. location 경고 문구 추가 (#15)

--- frontend/src/pages/LocationEditPage/index.js
+++ frontend/src/pages/LocationEditPage/index.js
@@ -1,11 +1,15 @@
+import './style.scss';
 import { setState } from '../../utils/globalObserver.js';
 import { pageState } from '../../store/page.js';
 import TopBar from '../../components/Common/TopBar';
 import { createElement } from '../../utils/dom';
+import LocationList from '../../components/LocationList';
 
 export default class LocationEditPage {
   constructor() {
     this.PAGE_TITLE = '내 동네 설정하기';
+    this.CAUTION_LINE1 = '지역은 최소 1개 이상';
+    this.CAUTION_LINE2 = '최대 2개까지 설정 가능해요.';
 
     this.$target = createElement({ tagName: 'div', classNames: ['page'] });
     this.setPageState = setState(pageState);
@@ -20,8 +24,16 @@ export default class LocationEditPage {
 
   render() {
     const topBar = new TopBar({ title: this.PAGE_TITLE }).$target;
+    const locationList = new LocationList().$target;
 
     this.$target.appendChild(topBar);
+    this.$target.innerHTML += `
+        <div class='location-cautions'>
+          <div>${this.CAUTION_LINE1}</div>
+          <div>${this.CAUTION_LINE2}</div>
+        </div>
+    `;
+    this.$target.appendChild(locationList);
   }
 
   handleClick() {}
