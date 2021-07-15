ccb9b38e92f775858703ffe7de6507a99fc2f941
eojin-dev <eojin-dev@naver.com>
2021-07-16 06:43:18 +0900

    chore. 불필요 코드 삭제

--- frontend/src/store/page.js
+++ frontend/src/store/page.js
@@ -1,5 +1,4 @@
 import HomePage from '../pages/HomePage';
-import CategoryPage from '../pages/CategoryPage';
 import { initState } from '../utils/globalObserver.js';
 
 export const pageState = initState({
