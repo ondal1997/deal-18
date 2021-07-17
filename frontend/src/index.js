000f10b08152e064895c84b14df3e35c3f259687
hayoung <hayoung@adminui-MacBookPro.local>
2021-07-17 15:31:25 +0900

    feat. 페이지 routes 적용 (#21)

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,24 @@
 import './style/style.scss';
 import App from './App.js';
+import Router from './lib/route';
+import { pageState } from './store/page';
+import HomePage from './pages/HomePage';
+import CategoryPage from './pages/CategoryPage';
+import LoginPage from './pages/LoginPage';
+import RegisterPage from './pages/RegisterPage';
+import LocationEditPage from './pages/LocationEditPage';
 
 const $root = document.querySelector('#root');
+
+const routes = {
+  '/': HomePage,
+  '/categories': CategoryPage,
+  '/login': LoginPage,
+  '/register': RegisterPage,
+  '/location': LocationEditPage,
+};
+
+export const router = new Router({ routes, pageState });
 
 const app = new App();
 
