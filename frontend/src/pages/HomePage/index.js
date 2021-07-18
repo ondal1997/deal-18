2ff984f9d4babb452ead5679af6c4ab6a5ee6d2f
hayoung <hayoung@adminui-MacBookPro.local>
2021-07-18 15:26:43 +0900

    feat. 상품 등록하기 버튼 (#25)

--- frontend/src/pages/HomePage/index.js
+++ frontend/src/pages/HomePage/index.js
@@ -1,18 +1,22 @@
+import './style.scss';
 import { setState } from '../../utils/globalObserver.js';
 import { pageState } from '../../store/page.js';
 import { createElement } from '../../utils/dom.js';
 import MainTopBar from '../../components/MainTopBar';
 import ProductList from '../../components/ProductList';
+import postButton from '../../../public/assets/homepage/postButton.svg';
 
 export default class HomePage {
   constructor() {
-    this.$target = createElement({ tagName: 'div', classNames: ['page'] });
+    this.$target = createElement({ tagName: 'div', classNames: ['page', 'home-page'] });
+    this.$postBtn = createElement({ tagName: 'div', classNames: ['post-btn'] });
     this.setPageState = setState(pageState);
 
     this.init();
   }
 
   init() {
+    this.renderPostBtn();
     this.render();
   }
 
@@ -22,5 +26,12 @@ export default class HomePage {
     this.$target.innerHTML = '';
     this.$target.appendChild(topBar.$target);
     this.$target.appendChild(productList.$target);
+    this.$target.appendChild(this.$postBtn);
+  }
+
+  renderPostBtn() {
+    this.$postBtn.innerHTML = `
+      <img src=${postButton} alt ='상품 등록하기 버튼' />
+    `;
   }
 }
