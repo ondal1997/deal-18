a74396bf74dad28db6401a7884ba8e684976268a
hayoung <hayoung@adminui-MacBookPro.local>
2021-07-18 20:31:37 +0900

    feat. 이미지 리스트 목데이터 상태 (#25)

---
+++ frontend/src/store/postPage.js
@@ -0,0 +1,14 @@
+import { initState } from '../utils/globalObserver';
+
+//목데이터
+/**
+ * 서버로 등록하고 url을 받은 뒤에 작업해야 될 듯!
+ */
+import testImg0 from '../../public/img/ImageLarge-0.png';
+import testImg1 from '../../public/img/ImageLarge-1.png';
+import testImg2 from '../../public/img/ImageLarge-2.png';
+
+export const uploadedImgState = initState({
+  key: '',
+  defaultValue: [testImg0, testImg1, testImg2],
+});
