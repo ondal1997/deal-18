64dcf88ded88a4194b44a831db6081357b1ffbc3
hayoung <hayoung@adminui-MacBookPro.local>
2021-07-19 00:17:34 +0900

    feat. form-img-list 클릭 이벤트 기본 틀 작성 (#25)

--- frontend/src/components/PostProductForm/FormImgList.js
+++ frontend/src/components/PostProductForm/FormImgList.js
@@ -2,7 +2,7 @@ import './style.scss';
 import { createElement } from '../../utils/dom';
 import pictureIcon from '../../../public/assets/postPage/pictureIcon.svg';
 import imgDeleteButton from '../../../public/assets/postPage/imgDeleteButton.svg';
-import { getState } from '../../utils/globalObserver';
+import { getState, setState, subscribe } from '../../utils/globalObserver';
 import { uploadedImgState } from '../../store/postPage';
 
 export default class FormImgList {
@@ -10,11 +10,19 @@ export default class FormImgList {
     this.MAX_IMG = 10;
     this.$target = createElement({ tagName: 'div', classNames: ['form-img-list'] });
 
+    this.setUploadedImg = setState(uploadedImgState);
     this.init();
   }
   init() {
+    subscribe(uploadedImgState, this.render.bind(this));
     this.render();
+    this.addEvent();
   }
+
+  addEvent() {
+    this.$target.addEventListener('click', this.handleClick.bind(this));
+  }
+
   render() {
     const uploadedImg = getState(uploadedImgState);
     const imgElements = uploadedImg.reduce((acc, src) => (acc += this.getImgElement(src)), '');
@@ -37,5 +45,33 @@ export default class FormImgList {
         </div>
       </div>
     `;
+  }
+
+  handleClick({ target }) {
+    if (this.isAddBtn(target)) {
+      if (this.hasMaxImg()) return;
+
+      //TODO: 서버 요청 후 this.setUploadedImg 실행
+
+      return;
+    }
+
+    if (this.isDeleteBtn(target)) {
+      //TODO 서버 요청 후 this.setUploadedImg
+      return;
+    }
+  }
+
+  isAddBtn(target) {
+    return !!target.closest('.form-img-add-btn');
+  }
+
+  isDeleteBtn(target) {
+    return !!target.closest('.form-img-delete-btn');
+  }
+
+  hasMaxImg() {
+    const uploadedImg = getState(uploadedImgState);
+    return uploadedImg.length === this.MAX_IMG;
   }
 }
