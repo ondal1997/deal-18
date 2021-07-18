7f657d6a3e0edb2617a32522058fe99e0795337b
hayoung <hayoung@adminui-MacBookPro.local>
2021-07-19 01:57:46 +0900

    feat. post-page 위치 목데이터로 마크업 및 스타일링 (#25)

--- frontend/src/pages/PostPage/index.js
+++ frontend/src/pages/PostPage/index.js
@@ -2,15 +2,19 @@ import './style.scss';
 import TopBar from '../../components/Common/CommonTopBar';
 import { createElement } from '../../utils/dom';
 import PostProductForm from '../../components/PostProductForm';
+import locationIcon from '../../../public/assets/postPage/locationIcon.svg';
 
 export default class Postpage {
   constructor() {
     this.PAGE_TITLE = '글쓰기';
 
     this.$target = createElement({ tagName: 'div', classNames: ['page', 'post-page'] });
+    this.$postLocation = createElement({ tagName: 'div', classNames: ['post-location'] });
+
     this.init();
   }
   init() {
+    this.renderLocation();
     this.render();
   }
   render() {
@@ -19,5 +23,13 @@ export default class Postpage {
 
     this.$target.appendChild(topBar);
     this.$target.appendChild(postProductForm);
+    this.$target.appendChild(this.$postLocation);
+  }
+  //TODO 목데이터 위치 수정
+  renderLocation() {
+    this.$postLocation.innerHTML = `
+          <img src=${locationIcon} alt='위치 아이콘' />
+          <div>역삼동</div>
+        `;
   }
 }
