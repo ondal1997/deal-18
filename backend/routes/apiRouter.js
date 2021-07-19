c58a9432305c359188bcec14474de23cb552d4d3
hayoung <hayoung@adminui-MacBookPro.local>
2021-07-20 00:05:56 +0900

    feat. apiRouter에 townRouter 등록 (#34)

--- backend/routes/apiRouter.js
+++ backend/routes/apiRouter.js
@@ -2,7 +2,9 @@ const express = require('express');
 const router = express.Router();
 
 const userRouter = require('./user');
+const townRouter = require('./town');
 
 router.use(userRouter);
+router.use(townRouter);
 
 module.exports = router;
