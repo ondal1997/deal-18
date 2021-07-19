172805776134ad59873fa383ab3d9b4433a147a0
hayoung <hayoung@adminui-MacBookPro.local>
2021-07-20 03:27:26 +0900

    chore. 목데이터 추가 구문 (#34)

--- backend/db.js
+++ backend/db.js
@@ -2,6 +2,8 @@ require('dotenv').config();
 const configs = require('../configs/config.dev.json');
 
 const mysql = require('mysql2');
+const { INSERT_PRODUCT } = require('./query/product');
+const { INSERT_CHAT } = require('./query/chat');
 
 const pool = mysql.createPool({
   host: process.env.DB_HOST || configs.database.host,
@@ -14,3 +16,66 @@ const pool = mysql.createPool({
 });
 
 module.exports = pool.promise();
+
+//insert product sample
+/*
+const k = [
+  {
+    title: 'test1',
+    price: 10000,
+    description: 'test1',
+    town: '강남',
+    user_id: 'kyle',
+    category: '디지털기기',
+  },
+  {
+    title: 'test2',
+    price: 158000,
+    description: 'test2',
+    town: '역삼동',
+    user_id: 'woowa',
+    category: '디지털기기',
+  },
+  {
+    title: 'test3',
+    price: 10000,
+    description: 'test3',
+    town: '강남',
+    user_id: 'kyle',
+    category: '생활가전',
+  },
+  {
+    title: 'test4',
+    price: 158000,
+    description: 'test4',
+    town: '역삼동',
+    user_id: 'woowa',
+    category: '생활가전',
+  },
+  {
+    title: 'test5',
+    price: 10000,
+    description: 'test5',
+    town: '강남',
+    user_id: 'kyle',
+    category: '생활가전',
+  },
+  {
+    title: 'test6',
+    price: 158000,
+    description: 'test6',
+    town: '역삼동',
+    user_id: 'woowa',
+    category: '유아동',
+  },
+];
+
+for (let x of k) {
+  pool.query(INSERT_PRODUCT(x));
+}
+
+
+
+ pool.query(INSERT_CHAT({ product_id: 92, seller_id: 'woowa', customer_id: 'kyle' }));
+
+ */
