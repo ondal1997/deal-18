a6bc2570ffd4b091bc3915fe92ab42f01d5f9cee
hayoung <hayoung@adminui-MacBookPro.local>
2021-07-20 00:52:52 +0900

    feat. apiRouter에 productRouter 등록 (#34)

--- backend/routes/apiRouter.js
+++ backend/routes/apiRouter.js
@@ -3,8 +3,10 @@ const router = express.Router();
 
 const userRouter = require('./user');
 const townRouter = require('./town');
+const productRouter = require('./product');
 
 router.use(userRouter);
 router.use(townRouter);
+router.use(productRouter);
 
 module.exports = router;
