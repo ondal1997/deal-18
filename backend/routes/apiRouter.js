bfc5f5f2f2b4141e5675bab7ae77d625bdf39c41
eojin-dev <eojin-dev@naver.com>
2021-07-20 23:42:26 +0900

    feat. 채팅방 리스트 조회, 채팅방 생성 api (#34)

--- backend/routes/apiRouter.js
+++ backend/routes/apiRouter.js
@@ -4,9 +4,11 @@ const router = express.Router();
 const userRouter = require('./user');
 const townRouter = require('./town');
 const productRouter = require('./product');
+const chatRouter = require('./chat');
 
 router.use(userRouter);
 router.use(townRouter);
 router.use(productRouter);
+router.use(chatRouter);
 
 module.exports = router;
