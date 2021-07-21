c257e601a767df80d091e5e11a98ccc590c44005
hayoung <hayoung@adminui-MacBookPro.local>
2021-07-21 16:30:02 +0900

    feat. 이미지 추가 api 연동 (#38)

--- frontend/src/components/PostProductForm/FormImgList/index.js
+++ frontend/src/components/PostProductForm/FormImgList/index.js
@@ -5,6 +5,10 @@ import imgDeleteButton from '../../../../public/assets/postPage/imgDeleteButton.
 import { getState, setState, subscribe } from '../../../utils/globalObserver';
 import { uploadedImgState } from '../../../store/postPage';
 
+const API = {
+  uploadImg: 'http://localhost:3000/upload',
+};
+
 export default class FormImgList {
   constructor() {
     this.MAX_IMG = 10;
@@ -20,11 +24,14 @@ export default class FormImgList {
   }
 
   addEvent() {
+    const input = this.$target.querySelector('input');
     this.$target.addEventListener('click', this.handleClick.bind(this));
+    input.addEventListener('input', this.handleInputImg.bind(this));
   }
 
   render() {
     const uploadedImg = getState(uploadedImgState);
+    console.log(uploadedImg.length);
     const imgElements = uploadedImg.reduce((acc, src) => (acc += this.getImgElement(src)), '');
 
     this.$target.innerHTML = `
@@ -32,14 +39,14 @@ export default class FormImgList {
           <img src=${pictureIcon} alt='이미지 추가버튼'/>
           <div class='form-img-count'>${uploadedImg.length}/${this.MAX_IMG}</div>
         </label>
-        <input type='file' id='input-file'/>
+        <input type='file' id='input-file' />
         ${imgElements}
       `;
   }
   getImgElement(src) {
     return `
       <div class='form-img'>
-        <img src=${src} alt='이미지' />
+        <img src=${src} alt='이미지'/>
         <div class='form-img-delete-btn'>
           <img src=${imgDeleteButton} alt='이미지 삭제 버튼' />
         </div>
@@ -48,18 +55,28 @@ export default class FormImgList {
   }
 
   handleClick({ target }) {
-    if (this.isAddBtn(target)) {
-      if (this.hasMaxImg()) return;
-
-      //TODO: 서버 요청 후 this.setUploadedImg 실행
-
-      return;
-    }
-
     if (this.isDeleteBtn(target)) {
       //TODO 서버 요청 후 this.setUploadedImg
       return;
     }
+  }
+
+  handleInputImg({ target: { files } }) {
+    const imageBlob = Object.values(files)[0]; //이미지 1개 전송
+
+    const formData = new FormData();
+    formData.append('upload', imageBlob);
+    this.uploadImg();
+  }
+
+  uploadImg() {
+    fetch(API.uploadImg, {
+      method: 'POST',
+      body: formData,
+    })
+      .then((res) => res.json())
+      .then((img) => this.setUploadedImg((imgs) => [...imgs, img.url]))
+      .catch((err) => console.log(err));
   }
 
   isAddBtn(target) {
