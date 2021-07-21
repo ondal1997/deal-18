5eb5cbb2fa157cad1755d4d0b1e1f647091a0a6c
ondal1997 <64543699+ondal1997@users.noreply.github.com>
2021-07-21 21:31:21 +0900

    chore. 대소문자 관련 문제

--- frontend/src/API/productAPI.js
+++ frontend/src/API/productAPI.js
@@ -19,3 +19,32 @@ export function fetchPostProduct(productInfo) {
     .then((res) => res.json())
     .catch(checkErrorFetchedJson);
 }
+
+export function fetchProductDetail(productId) {
+  return fetch(`/api/products/${productId}`)
+    .then((res) => res.json())
+    .then(checkErrorFetchedJson);
+}
+
+export function fetchToggleLike(productId, currentIsLiked) {
+  let method = currentIsLiked ? 'delete' : 'post';
+
+  return fetch(`/api/like/${productId}`, { method })
+    .then((res) => res.json())
+    .then(checkErrorFetchedJson);
+}
+
+export function fetchUpdateProductState(product, state) {
+  const json = Object.assign(product, { state });
+  return fetch(`/products/${product.id}`, {
+    method: 'PUT',
+    body: JSON.stringify(json),
+    headers: { 'Content-Type': 'application/json' },
+  })
+    .then((res) => res.json())
+    .then(checkErrorFetchedJson);
+}
+
+export function fetchDeleteProduct(productId) {
+  return fetch(`/products/${productId}`, { method: 'delete' }).then(((res) => res.json()).then(checkErrorFetchedJson));
+}
