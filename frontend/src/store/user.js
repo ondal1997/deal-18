d37cb9ed8b0c6734eb58a6eafb1a066de89e494b
eojin-dev <eojin-dev@naver.com>
2021-07-21 22:47:17 +0900

    fix. userState initState 형식 수정 (#42)

--- frontend/src/store/user.js
+++ frontend/src/store/user.js
@@ -1,7 +1,10 @@
 import { initState } from '../utils/globalObserver.js';
 
 export const userState = initState({
-  userId: null,
-  primaryTown: '역삼동',
-  towns: ['역삼동'],
+  key: 'userState',
+  defaultValue: {
+    userId: null,
+    primaryTown: '역삼동',
+    towns: ['역삼동'],
+  },
 });
