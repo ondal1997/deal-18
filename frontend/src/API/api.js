cadcbe3a8a85153ed8a108c45cbfbb268570229a
hayoung <hayoung@adminui-MacBookPro.local>
2021-07-21 18:38:43 +0900

    feat. API 분리

---
+++ frontend/src/API/api.js
@@ -0,0 +1,11 @@
+//env로 빼낼것
+const BASE_URL = 'http://localhost:3000';
+
+const API = {
+  GET_IMG_URL: BASE_URL + '/upload',
+  PRODUCT: '/api/products',
+  LIKE: '/api/like',
+  TOWN: '/api/towns',
+};
+
+export default API;
